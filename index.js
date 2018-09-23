df60766cda53f74c4b1159d17f0a8d5eb1a2689f
Alex Harwood <alex.harwood@live.co.uk>
2018-09-23 20:51:35 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,202 @@
+#!/usr/bin/env node
+
+const concat = require('concat');
+const request = require('request');
+const unzip = require('unzip');
+const jsonfile = require('jsonfile');
+const colors = require('colors');
+const fs = require('fs-extra');
+const uglify = require('uglify-js');
+
+const applicationVersion = "1.0.0";
+
+const packageDownloadUrl = "https://github.com/alexanderpharwood/jcache/archive/master.zip";
+const packageVersion = "1.0.0";
+
+
+var router = "";
+var controllers = "";
+var middleware = "";
+var views = "";
+
+var concatonated = "";
+
+const [,, ... args] = process.argv;
+
+function getRegisterViewCode(name, path){
+    var template = fs.readFileSync(path, "utf8");
+
+    //sanitise the template, encoding quote and removing line breaks.
+    template = template.replace(new RegExp("'", "g"), "&apos;");
+    template = template.replace(/(\r\n\t|\n|\r\t)/gm,"");
+
+    return "Sully.registerView('" + name + "', '" + template + "');\n\n";
+}
+
+console.log("\n");
+console.log("Sully.js ".blue + applicationVersion.blue);
+
+switch(args[0]){
+
+    case "build":
+
+     console.log("--> Starting build");
+
+    function errorAndExit(message){
+
+        var messageString = "ERROR: " + message;
+
+        console.log(messageString.red);
+
+        return;
+
+    }
+
+    //load the build.json file.
+    jsonfile.readFile('build.json', function(err, buildConfig) {
+
+        if(err){
+            return errorAndExit("The 'build.json' either has an error in it or can not be found");
+        }
+
+        //Compiling router
+        router = fs.readFileSync(buildConfig.router, "UTF8");
+
+        //Compile middleware (optional)
+
+        //build.json middleware element is present
+        if (typeof buildConfig.middleware !== "undefined"){
+
+            console.log("--> Compiling middleware");
+
+            var middlewareArray = [];
+
+            for (var middlewareName in buildConfig.middleware){
+                middlewareArray.push(buildConfig.middleware[middlewareName]);
+            }
+
+            concat(middlewareArray).then(function(middleware){
+
+
+                middleware = "(function(){" + middleware + "})();";
+
+                        //Compile views
+
+                        //build.json views element is present
+                        if (typeof buildConfig.views === "undefined"){
+                            return errorAndExit("build.json' does not contain a 'views' element");
+                        }
+
+                        console.log("--> Compiling views");
+
+                        var views = "(function(){";
+
+                        for (var viewName in buildConfig.views){
+                            views += getRegisterViewCode(viewName, buildConfig.views[viewName]);
+                        }
+
+                        views += "})();";
+
+                        //Compile controllers
+
+                        //build.json controllers element is present
+                        if (typeof buildConfig.controllers === "undefined"){
+                            return errorAndExit("build.json' does not contain a 'controllers' element");
+                        }
+
+                        console.log("--> Compiling controllers");
+
+                        var controllersArray = [];
+
+                        for (var controllerName in buildConfig.controllers){
+                            controllersArray.push(buildConfig.controllers[controllerName]);
+                        }
+
+                        concat(controllersArray).then(function(controllers){
+
+                            controllers = "(function(){" + controllers + "})();";
+
+                            concatonated = router + controllers + middleware + views;
+
+                            //If this is a production build
+                            if(args[1] === "--prod"){
+
+                                console.log("--> Compiling production build: " + buildConfig.builds.production.output + " (minified)");
+
+                                var concatonated = "(function(){" + uglify.minify(concatonated).code + "})();";
+
+                                fs.writeFileSync(buildConfig.builds.production.output, concatonated);
+
+                            } else {
+
+                                console.log("--> Compiling development build: " + buildConfig.builds.development.output + " (uncompressed)");
+
+                                fs.writeFileSync(buildConfig.builds.development.output, concatonated);
+
+                            }
+
+                            console.log("Build finished successfully!".green);
+                            console.log("\n");
+
+                        });
+
+            });
+
+        }
+
+    });
+
+
+        break;
+
+    case "new":
+
+        if (typeof args[1] === "undefined"){
+            return errorAndExit("No project name specified");
+        }
+
+        console.log("--> creating new project: " + args[1]);
+
+        console.log("--> Downloading package from: " + packageDownloadUrl);
+
+        var downloadStream = request(packageDownloadUrl).pipe(fs.createWriteStream("sully-" + packageVersion + ".zip"));
+        downloadStream.on('error', function(err){
+            return errorAndExit(err);
+        });
+
+        downloadStream.on('finish', function(response){
+
+            console.log("--> Download finished");
+
+            console.log("--> Extracting");
+
+            if (fs.existsSync(args[1])){
+                return errorAndExit("A directory already exists with that name");
+            }
+
+            fs.createReadStream("sully-" + packageVersion + ".zip").pipe(unzip.Extract({ path: "." }));
+
+            console.log("--> Extract finished");
+            console.log(args[1].green + " created successfully!".green);
+            console.log("\n");
+
+        });
+
+        break;
+
+    default:
+
+        console.log("Usage: sullyjs <command>");
+        console.log("\n");
+
+        console.log("     new <project-name>     " + "Create a new project");
+        console.log("\n");
+
+        console.log("     build <options>     " + "   Build the project in the current directory");
+        console.log("                            Options:");
+        console.log("                               --prod  (specifies a production build)");
+        console.log("\n");
+
+        break;
+
+}
